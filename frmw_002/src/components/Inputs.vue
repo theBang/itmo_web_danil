04605f22a5b2b48d94566e601a6ccc5b47cc4230
theBang <d.danil.s.96@gmail.com>
2019-06-16 17:13:37 +0500

    Add files via upload

---
+++ frmw_002/src/components/Inputs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Два связанных текстовых поля (FRMW_002)</h1>
+    <h2>Данил Демашов</h2>
+    <input v-model="text"><br>
+    <input v-model="text">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style scoped>
+  input {
+    margin: 10px
+  }
+</style>
