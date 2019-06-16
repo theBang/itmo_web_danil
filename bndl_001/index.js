9d80db4bc65662d00ae904d8776bbd8829225471
theBang <d.danil.s.96@gmail.com>
2019-06-16 16:14:33 +0500

    Add files via upload

---
+++ bndl_001/index.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+
+const t = moment().format('DD/MM/YYYY HH:mm');
+document.body.innerHTML = `<h1>Сейчас ${t}</h1>`;
+document.body.innerHTML += `<h2>${top.location.href}</h2>`;
